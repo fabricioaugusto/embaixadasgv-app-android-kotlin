8c4f76802a80984e9deee3592e3d11c4c7e66237
Fabrício Augusto <fabricio.mata@gmail.com>
2019-08-08 00:39:15 -0300

    Criado a tela de criar eventos

---
+++ app/src/main/java/com/balloondigital/egvapp/activity/Create/CreateEventActivity.kt
@@ -0,0 +1,13 @@
+package com.balloondigital.egvapp.activity.Create
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.balloondigital.egvapp.R
+
+class CreateEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
